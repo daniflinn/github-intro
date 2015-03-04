302b1eb74a46d80066a5c6a163a350fd279534f9
daniflinn <danigirlflinn@gmail.com>
2015-03-04 13:14:32 -0800

    Update Blink.ino

--- arduino/Blink/Blink.ino
+++ arduino/Blink/Blink.ino
@@ -1,15 +1,15 @@
 // this is my blink sketch.  Is it going to work?
-int led = 13; //set pin 13 as output
+int led = 13; //"led" to pin 13
 
 void setup() {                
 
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);  //sets led (pin 13) as an output   
 }
 
 
 void loop() {
-  digitalWrite(led, HIGH);   
-  delay(1000);              
+  digitalWrite(led, HIGH);   //led lights up
+  delay(1000);              //delays 1 second 
   digitalWrite(led, LOW);    
   delay(1000);              
 }
