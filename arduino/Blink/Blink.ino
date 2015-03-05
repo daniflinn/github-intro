1d9f312cbc8e3d2185a2f92be9e37ee5aa572e5d
daniflinn <danigirlflinn@gmail.com>
2015-03-05 10:58:57 -0800

    I added comments.

--- arduino/Blink/Blink.ino
+++ arduino/Blink/Blink.ino
@@ -1,15 +1,19 @@
-// this is my blink sketch.  Is it going to work?
-int led = 13; //"led" to pin 13
+// this is my blink sketch.  Is it going to work? 
+// Yes it will, if you want an LED on pin 13 to blink for one
+// second thn wait one second befoer blinking again, and then repeat forever
 
-void setup() {                
+int led = 13; // declares "led" to be on pin 13
+
+void setup() {     //sets inputs and outputs           
 
   pinMode(led, OUTPUT);  //sets led (pin 13) as an output   
 }
 
 
-void loop() {
+void loop() {                //declares a loop which will continue repeating each 
+                             //comanded in order over and over agian
   digitalWrite(led, HIGH);   //led lights up
-  delay(1000);              //delays 1 second 
-  digitalWrite(led, LOW);    
-  delay(1000);              
+  delay(1000);              //delays 1 second before completing the next comande
+  digitalWrite(led, LOW);   //led goes off  
+  delay(1000);              //delays 1 second before repeating loop
 }
